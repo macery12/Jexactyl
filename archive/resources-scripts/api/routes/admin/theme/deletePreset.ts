c73c9c54004bbfbbddbc5d27dbb06f1766eae00c
macery12 <macen.elliott@gmail.com>
2026-09-07 23:19:29 +0000

    Remove legacy archive from version control

--- archive/resources-scripts/api/routes/admin/theme/deletePreset.ts
+++
@@ -1,5 +0,0 @@
-import http from '@/api/http';
-
-export default (id: number): Promise<void> => {
-    return http.delete(`/api/application/theme/presets/${id}`).then(() => undefined);
-};
--- archive/resources-scripts/api/routes/admin/theme/savePreset.ts
+++
@@ -1,6 +0,0 @@
-import http from '@/api/http';
-import { ThemePreset } from './getPresets';
-
-export default (name: string): Promise<ThemePreset> => {
-    return http.post('/api/application/theme/presets', { name }).then(({ data }) => data.preset);
-};
